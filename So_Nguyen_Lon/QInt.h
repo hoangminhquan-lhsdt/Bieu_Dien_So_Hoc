0fa6d473c61fb8de478ca248906085853282efb6
laohacsieudeptrai <laohacsieudeptrai@gmail.com>
2019-03-08 16:50:44 +0000

    upload c++ files

---
+++ So_Nguyen_Lon/QInt.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+class QInt {
+private:
+	unsigned int data[4];
+public:
+	QInt();
+	~QInt();
+	friend istream& operator>>(istream & in, QInt & N);
+	friend ostream& operator<<(ostream & out, const QInt & N);
+	QInt operator+(const QInt & N);
+	QInt operator-(const QInt & N);
+
+};
---
+++ So_Nguyen_Lon/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+#include "QInt.h"
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
