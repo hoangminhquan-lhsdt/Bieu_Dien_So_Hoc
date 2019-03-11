cd5b5eb4f0cc5ab1abfacda6a8c5822476435b86
NHẬT TRẦN MINH <1712635@student.hcmus.edu.vn>
2019-03-11 16:46:51 +0700

    Decstr to BinStr

--- So_Nguyen_Lon/main.cpp
+++ So_Nguyen_Lon/main.cpp
@@ -1,11 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 
 #include "QInt.h"
 
 using namespace std;
-
+string DecStrToBinStr(string x)
+{
+	string Result,Temp,p; //p thuong
+	char buffer[10];
+	while (x != "0")
+	{
+		Temp.clear();
+		p.clear();
+		Result = _itoa((x[x.length() - 1] - '0') % 2,buffer,10)+Result;
+		for (int i = 0; i < x.length(); i++)
+		{
+			Temp += x[i];
+			p += _itoa(stoi(Temp) / 2, buffer, 10);
+			Temp = _itoa(stoi(Temp) % 2, buffer, 10);
+		}
+		while (p[0] == '0' && p.length() != 1)
+		{
+			p.erase(0,1);
+		}
+		x = p;
+	}
+	return Result;
+}
 int main() {
 
+	cout << DecStrToBinStr("1234235235235244534636546346474576346346376785623546493232")<<endl;
+	system("pause");
 	return 0;
 }
