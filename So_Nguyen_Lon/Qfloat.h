311ba4bd013cbe4e59c087363bbe48462f25411c
diephieunghia123 <38271721+diephieunghia123@users.noreply.github.com>
2019-03-17 00:36:47 +0700

    1.Add Qfloat(string c): chuyển từ thập phân ra nhị phân. 2. VẪN CHƯA GIẢM ĐỘ DÀI CỦA CHUỖI.

--- So_Nguyen_Lon/Qfloat.h
+++ So_Nguyen_Lon/Qfloat.h
@@ -1,8 +1,18 @@
-#pragma once
+#include <iostream>
+using namespace std;
+
 class Qfloat
 {
+private:
+	unsigned int data[4];
 public:
 	Qfloat();
+	Qfloat(string x);
 	~Qfloat();
+	friend ostream& operator<<(ostream& os, const Qfloat d);
 };
-
+string ThapPhan(string x);
+int ReExpo(string x, int &k);
+string QuadPres(string nhiphan);
+string QuadExpo(int number);
+string Tra2(int a);
