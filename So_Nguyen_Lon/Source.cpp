1c017bdb5f505806f4de552f56dab1a279d5474b
NHẬT TRẦN MINH <1712635@student.hcmus.edu.vn>
2019-03-25 21:36:15 +0700

    Revert "Sửa format và lỗi dấu Source.cpp"
    This reverts commit 6240d2e02735197daceb70a536b2f4202a0c3e5d.

--- So_Nguyen_Lon/Source.cpp
+++ So_Nguyen_Lon/Source.cpp
@@ -10,9 +10,7 @@ int BinDec(string x)
 {
 	int s = 0, n = x.length();
 	for (int i = 0; i < n; i++)
-	{
 		s += (x[i] - '0')*pow(2, n - 1 - i);
-	}
 	return s;
 }
 
@@ -83,7 +81,6 @@ string Tra2(int a)
 	reverse(tra.begin(), tra.end());
 	return tra;
 }
-
 string Tra1016(unsigned int a)
 {
 	int sodu;
@@ -93,27 +90,13 @@ string Tra1016(unsigned int a)
 		sodu = a % 16;
 		switch (sodu)
 		{
-		case 10:
-			tra.push_back('A');
-			break;
-		case 11:
-			tra.push_back('B');
-			break;
-		case 12:
-			tra.push_back('C');
-			break;
-		case 13:
-			tra.push_back('D');
-			break;
-		case 14:
-			tra.push_back('E');
-			break;
-		case 15:
-			tra.push_back('F');
-			break;
-		default:
-			tra.push_back(sodu + '0');
-			break;
+		case 10: tra.push_back('A'); break;
+		case 11: tra.push_back('B'); break;
+		case 12: tra.push_back('C'); break;
+		case 13: tra.push_back('D'); break;
+		case 14: tra.push_back('E'); break;
+		case 15: tra.push_back('F'); break;
+		default: tra.push_back(sodu + '0'); break;
 		}
 		a = a / 16;
 	}
@@ -136,8 +119,7 @@ int checkstatusdigits(string &a)
     return 1; // số dương
 }
 
-void removezero(string &a) 
-{
+void removezero(string &a) {
 	int i = 0;
 	while (a[0] == '0')
 	{
@@ -149,9 +131,7 @@ void removezero(string &a)
 		if (a[1] == '.')
 			break;
 		else
-		{
 			a.erase(0, 1);
-		}
 	}
 	int n = a.length() - 1;
 	if (a.find('.') != -1)
@@ -160,11 +140,11 @@ void removezero(string &a)
 			if (a[n - 1] == '.')
 			{
 				a.erase(n);
+
 				a.erase(n - 1);
 				break;
 			}
-			else 
-			{
+			else {
 				a.erase(n);
 				n--;
 			}
@@ -205,9 +185,7 @@ void insertdot(string &a, int x) //Chèn dấu chấm vào trước x số ( tí
 	if (n < x)
 	{
 		for (int i = n; i < x; i++)
-		{
 			a.insert(a.begin(), '0');
-		}
 		a.insert(a.begin(), '.');
 		a.insert(a.begin(), '0');
 	}
@@ -215,7 +193,8 @@ void insertdot(string &a, int x) //Chèn dấu chấm vào trước x số ( tí
 
 int Compare(string s1, string s2)
 {
-	string a = s1, b = s2;
+	string a = s1;
+	string b = s2;
 	if ((a[0] == '-'))
 	{
 		if (b[0] == '-')
@@ -266,14 +245,18 @@ int Compare(string s1, string s2)
 	{
 		return -1;
 	}
+
+
 	int n;
 	bool temp = true;
+
 	if (a.length() > b.length())
 	{
 		n = b.length();
 		temp = false;
 	}
 	else n = a.length();
+
 	for (int i = 0; i < n; i++)
 	{
 		if (a[i] < b[i])
@@ -289,6 +272,7 @@ int Compare(string s1, string s2)
 	{
 		return 0;
 	}
+
 	if (temp)
 	{
 		return -1;
@@ -296,9 +280,10 @@ int Compare(string s1, string s2)
 }
 string Sum(string s1, string s2) // Số nguyên 
 {
-	string a = s1, b = s2, c;
+	string a = s1;
+	string b = s2;
 	char  sumchar;
-
+	string c;
 	int x1 = removedot(a), x2 = removedot(b), x3, x4, i;
 	if (x1 > x2)
 	{
@@ -310,8 +295,7 @@ string Sum(string s1, string s2) // Số nguyên
 			//chenkitutrongchuoi(b, strlen(b), '0');
 		}
 	}
-	else
-	{
+	else {
 		x3 = x2;
 		x4 = x2 - x1;
 		for (i = 1; i <= x4; i++)
@@ -339,6 +323,7 @@ string Sum(string s1, string s2) // Số nguyên
 			//chenkitutrongchuoi(a, 0, '0');
 		}
 	}
+	// 
 	int n = a.length();
 	if (y1 == y2) // Cùng dấu 
 	{
@@ -354,10 +339,12 @@ string Sum(string s1, string s2) // Số nguyên
 			count = sum / 10;
 			sumchar = itoc(sum % 10);
 			c = sumchar + c;
+			//chenkitutrongchuoi(c, 0, sumchar);
 		}
 		removezero(c);
 		if (y1 == 0)
 			c = '-' + c;
+		//chenkitutrongchuoi(c, 0, '-');
 		insertdot(c, x3);
 		removezero(c);
 		return c;
@@ -381,17 +368,15 @@ string Sum(string s1, string s2) // Số nguyên
 					sum += 10;
 				}
 				else
-				{
 					count = 0;
-				}
 				sumchar = itoc(sum % 10);
 				c = sumchar + c;
+				//chenkitutrongchuoi(c, 0, sumchar);
 			}
 			removezero(c);
 			if (y1 == 0 && !(c[0] == '0'&& c.length() == 1))
-			{
 				c = '-' + c;
-			}
+			//chenkitutrongchuoi(c, 0, '-');
 			insertdot(c, x3);
 			removezero(c);
 			return c;
@@ -399,15 +384,16 @@ string Sum(string s1, string s2) // Số nguyên
 		else //a<b
 		{
 			a = '1' + a;
+			//chenkitutrongchuoi(a, 0, '1');
 			c = Subtract(a, b);
 			if (a.length() == c.length())
 			{
 				c.erase(0, 1);
+				//xoakitutrongchuoi(c, 0);
 				removezero(c);
 				if (y1 == 0 && !(c[0] == '0'&& c.length() == 1))
-				{
 					c = '-' + c;
-				}
+				//chenkitutrongchuoi(c, 0, '-');
 				insertdot(c, x3);
 				removezero(c);
 				return c;
@@ -418,12 +404,13 @@ string Sum(string s1, string s2) // Số nguyên
 				int j = c.length();
 				for (i = 1; i <= j; i++)
 					d = '0' + d;
+				//chenkitutrongchuoi(d, 0, '0');
 				d = '1' + d;
+				//chenkitutrongchuoi(d, 0, '1');
 				c = Subtract(d, c);
 				if (y1 == 1 && !(c[0] == '0'&& c.length() == 1))
-				{
 					c = '-' + c;
-				}
+				//chenkitutrongchuoi(c, 0, '-');
 				insertdot(c, x3);
 				removezero(c);
 				return c;
@@ -452,7 +439,8 @@ string Multiply(string s1, string s2)
 	int y1 = checkstatusdigits(a), y2 = checkstatusdigits(b);
 	int x1 = removedot(a), x2 = removedot(b);
 	int reb = 0, i, j, Sum = 0, n1, n2, x, y;
-	n1 = a.length(), n2 = b.length();
+	n1 = a.length();
+	n2 = b.length();
 	for (i = 1; i <= (n1 + n2); i++)
 	{
 		Sum = reb;
@@ -465,6 +453,7 @@ string Multiply(string s1, string s2)
 				Sum += x * y;
 			}
 		}
+
 		reb = Sum / 10;
 		sumchar = itoc(Sum % 10);
 		c = sumchar + c;
@@ -489,33 +478,38 @@ string DivideInteger(string a, string b) //Chia lấy phần nguyên
 	c = '0' + c;
 	while (i < n)
 	{
+
 		d.push_back(a[i]);
+
 		x = Compare(d, b);
 		if (x == -1)
 		{
 			if (d == "0" && a[i] == '0')
 				c.push_back('0');
-			else if (i == n - 1)
-			{
-				count = 0;
-				while (x != -1)
+			else
+				if (i == n - 1)
 				{
-					d = Subtract(d, b);
-					count++;
-					x = Compare(d, b);
+					count = 0;
+					while (x != -1)
+					{
+						d = Subtract(d, b);
+						count++;
+						x = Compare(d, b);
+					}
+
+					c.push_back(itoc(count));
 				}
-				c.push_back(itoc(count));
-			}
 			i++;
 			continue;
-		}
-		count = 0;
+		}count = 0;
 		while (x != -1)
 		{
 			d = Subtract(d, b);
 			count++;
 			x = Compare(d, b);
+
 		}
+
 		c.push_back(itoc(count));
 		i++;
 	}
@@ -546,7 +540,9 @@ string Divide(string a, string b)
 	{
 		return ("Error: So chia bang 0.");
 	}
-	string c = DivideInteger(a, b), e = DivideRemainder(a, b), d;
+	string d;
+	string c = DivideInteger(a, b);
+	string e = DivideRemainder(a, b);
 	int count = 0;
 	while (Compare(e,"0") != 0 && count < 128)
 	{
@@ -567,21 +563,19 @@ string Divide(string a, string b)
 string Exponential(string a, int n)
 {
 	string c = a;
+
 	int i;
+
 	if (n == 0)
 		return (string)("1");
 	for (i = 1; i < abs(n); i++)
-	{
 		c = Multiply(c, a);
-	}
 	if (n > 0)
-	{
 		return c;
-	}
 	else
-	{
 		return Divide("1", c);
-	}
+	//else
+	//	return Divide((char*)("1"), c);
 }
 
 string HexToBin(const string &x)
@@ -591,13 +585,9 @@ string HexToBin(const string &x)
 	for (int i = 0; i < x.length(); i++)
 	{
 		if (x[i] >= '0' && x[i] <= '9')
-		{
 			kq += temp[x[i] - '0'];
-		}
-		if (x[i] >= 'A' && x[i] <= 'F')
-		{
+		if (x[i] >= 'A' && x[i]<='F')
 			kq += temp[x[i] - 'A' + 10];
-		}
 	
 	}
 	return kq;
@@ -608,14 +598,10 @@ bool checkBase10(const string &x)
 
 	int i = 0;
 	if (x[i] == '-')
-	{
 		i++;
-	}
 	for (i; i < x.length(); i++)
-	{
 		if (x[i] > '9' || x[i] < '0')
 			return false;
-	}
 	return true;
 }
 
@@ -623,11 +609,12 @@ bool checkBase10(const string &x)
 string HaiMuN_Am(int n)
 {
 	if (n >= 0)
-	{
 		return string();
-	}
-	n = abs(n);// kq : 2^-n = 1/2^n
+	n = abs(n);
+	// kq : 2^-n = 1/2^n
 	string pow = Exponential("2", n);
+
+
 	string sobichia("1");
 	string sochia = pow;
 	pow.clear();
@@ -636,6 +623,7 @@ string HaiMuN_Am(int n)
 	{
 		if (smallerThan(sobichia, sochia))
 			sobichia += '0';
+
 		string i = "0";
 		string kq_temp;
 		int j = 0;
@@ -654,12 +642,16 @@ string HaiMuN_Am(int n)
 				//cỏn i = i-1 nhưng do chuỗi nên khỏi làm cho mệt
 			}
 		}
+
 		thuong += itoc(j);
 		string thuong_nhan_sochia = Multiply(thuong.substr(thuong.length() - 1, 1), sochia);
 		removezero(thuong_nhan_sochia);
 		sobichia = Sub(sobichia, thuong_nhan_sochia);// số bi chia= sobichia -thuong*sochia
+
 	}
+
 	thuong = "0." + thuong;
+	//cout << thuong << endl;
 	return thuong;
 }
 
@@ -689,9 +681,7 @@ string Sub(string a, string b)// a b là chuỗi số dương
 	removezero(a);
 	removezero(b);
 	if (a == b)
-	{
 		return string("0");
-	}
 	char sign = '+';
 	string sobitru = a, sotru = b;
 	if (smallerThan(a, b)) // nếu a < b--> kq sẽ âm
@@ -717,11 +707,8 @@ string Sub(string a, string b)// a b là chuỗi số dương
 		else
 		{
 			if (muon == 0)
-			{
 				temp_kq = 0;
-			}
-			else 
-			{
+			else {
 				temp_kq = 9;
 				muon = 1;
 			}
@@ -730,6 +717,8 @@ string Sub(string a, string b)// a b là chuỗi số dương
 	}
 	if (sign == '-')
 		kq = '-' + kq;
+	//cout << kq << endl;
 	removezero(kq);
-	return kq;
+	return kq
+		;
 }
